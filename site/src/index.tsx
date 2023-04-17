b1e749f593b0c833802614cd4c7f01a9834a858b
emallson <emallson@emallson.net>
2023-04-16 20:15:15 -0400

    remove sparkline components

--- site/src/index.tsx
+++ site/src/index.tsx
@@ -1,86 +1,14 @@
 import { render } from "solid-js/web";
-import { BetaParams, fit, pdf } from "./beta";
-import { styled } from "@macaron-css/solid";
 
-import "../resources/sparkline-font/font-faces.css";
-import { Show, createMemo } from "solid-js";
+import { Show } from "solid-js";
 import {
   SavedVariablesProvider,
   useSavedVariables,
 } from "./SavedVariablesContext";
 import FilePickerPage from "./FilePickerPage";
-import EncounterSelector, { useSelectedRecording } from "./EncounterSelector";
+import EncounterSelector from "./EncounterSelector";
 import EncounterDetails from "./EncounterDetails";
 import ScriptList from "./ScriptList";
-
-const sparklineWeights = {
-  Bar: ["Narrow", "Medium", "Wide", "Extrawide"],
-  Dot: ["Extrasmall", "Small", "Medium", "Large", "Extralarge"],
-  Dotline: ["Extrathin", "Thin", "Medium", "Thick", "Extrathick"],
-};
-
-const sparklineVariants = {
-  type: Object.fromEntries(
-    Object.keys(sparklineWeights).map((key) => [key, {}])
-  ),
-  weight: Object.fromEntries(
-    Object.values(sparklineWeights).flatMap((x) =>
-      x.map((weight) => [weight, {}])
-    )
-  ),
-};
-
-const sparklineCompoundVariants = Object.entries(sparklineWeights).flatMap(
-  ([chartType, weights]) =>
-    weights.map((weight) => ({
-      variants: {
-        type: chartType,
-        weight,
-      },
-      style: {
-        fontFamily: `Sparks-${chartType}-${weight}`,
-      },
-    }))
-);
-
-const Sparkline = styled("span", {
-  base: { fontVariantLigatures: "normal" },
-  variants: sparklineVariants,
-  compoundVariants: sparklineCompoundVariants,
-});
-
-const pdfPoints: number[] = [];
-
-const jitter = 0.01;
-const width = 0.05;
-
-for (let i = 0; i <= 1; i += width) {
-  if (i === 0) {
-    pdfPoints.push(jitter);
-  } else if (i === 1) {
-    pdfPoints.push(1 - jitter);
-  } else {
-    pdfPoints.push(i);
-  }
-}
-const BetaPDF = (params: BetaParams) => {
-  const contents = createMemo(() => {
-    const points: number[] = pdfPoints
-      .map((p) => p * params.max)
-      .map(pdf.bind(null, params));
-    const max = Math.max.apply(null, points);
-    const scaled = points
-      .map((point) => Math.round((point / max) * 100).toFixed(0))
-      .join(",");
-    return `{${scaled}}`;
-  });
-
-  return (
-    <Sparkline type="Dotline" weight="Medium">
-      {contents()}
-    </Sparkline>
-  );
-};
 
 const LoadingBlocker = () => {
   const { store } = useSavedVariables();

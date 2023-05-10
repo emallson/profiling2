2f38d137024baa4d0ef18a4516a61a683a529fb5
emallson <emallson@emallson.net>
2023-05-10 12:08:06 -0400

    fix build

--- site/wasm/build.rs
+++ site/wasm/build.rs
@@ -5,6 +5,7 @@ use std::fs;
 
 fn main() {
     let schema = schemars::schema_for!(types::Recording);
+    fs::create_dir_all("pkg").unwrap();
     fs::write(
         "pkg/schema.json",
         serde_json::to_string_pretty(&schema).unwrap(),
